9ccabb7b025860dc044e0da9722bac5ea2e5e829
Carlos Orue <carlos@orue.io>
2024-11-14 22:56:54 -0600

    add Network aliases

--- aliases.zsh
+++ aliases.zsh
@@ -4,6 +4,8 @@ alias zsh-update-plugins="find "$ZDOTDIR/plugins" -type d -exec test -e '{}/.git
 
 # Generate Password
 alias pw='openssl rand -base64 24 | tr -dc "A-Za-z0-9!@#$%^&*()_+" | head -c 20; echo'
+#Homebrew Update
+alias brewup='brew update; brew upgrade; brew upgrade --cask; brew autoremove; brew cleanup; brew doctor'
 
 # Terminal
 alias c='clear'
@@ -14,29 +16,18 @@ alias r='source $ZDOTDIR/.zshrc'
 alias edit='nvim'                                        # Open a file in Sublime Text
 alias hc='echo "" > $HOME/.zsh_history & exec $SHELL -l' # Clear history
 
-# git open repository on browser
-alias ghweb="gh repo view --web"
-
-# Create and open Markdown file on VSCode
-alias -s md='code -n --profile "Default"'
+alias ghweb="gh repo view --web"                  # git open repository on browser
+alias -s md='code -n --profile "Default"'         # Create and open Markdown file on VSCode
+alias ssh='TERM="xterm-256color" ssh'             # Remote Servers SSH connection with 256 colors
+alias ping='ping -c 5'                            # Ping 5 times
+alias uuid='uuidgen | tr "[:upper:]" "[:lower:]"' # Generate a uuid-v4
+alias df='df -h'                                  # Human Readable disk usage
 
 # Editor lauchers
 alias vim='/opt/homebrew/bin/nvim'
 alias pych="open -b com.jetbrains.pycharm ."
 alias vco="code ."
 alias vc="code"
-
-# Remote Servers SSH connection with 256 colors
-alias ssh='TERM="xterm-256color" ssh'
-
-# Ping 5 times
-alias ping='ping -c 5'
-
-# Generate a uuid-v4
-alias uuid='uuidgen | tr "[:upper:]" "[:lower:]"'
-
-# Human Readable disk usage
-alias df='df -h'
 
 # eza - ls alternative
 alias ls='eza --icons=always -F -H --group-directories-first --git -1'
@@ -45,13 +36,15 @@ alias ll="eza -aghlH --group-directories-first --git --git-repos --icons=always 
 tree() { eza -agTF --tree --icons --group-directories-first --git-ignore --git --git-repos --level="${1:-1}"; }
 
 # File operations
-alias cp='cp -iv' # Preferred 'cp' implementation
-alias mv='mv -iv' # Preferred 'mv' implementation
-alias rm='rm -i'
-alias rmrf='rm -rf'
+alias cp='cp -iv'                     # Preferred 'cp' implementation
+alias mv='mv -iv'                     # Preferred 'mv' implementation
+alias rm='rm -i'                      # Preferred 'rm' implementation
+alias rmrf='rm -rfI'                  # Preferred 'rm -rf' implementation
 alias f='open -a Finder ./'           # Opens current directory in MacOS Finder
 mcd() { mkdir -pv "$1" && cd "$1"; }  # Makes new Dir and jumps inside
 trash() { command mv "$@" ~/.Trash; } # Moves a file to the trash
 
-#Homebrew Update
-alias brewup='brew update; brew upgrade; brew upgrade --cask; brew autoremove; brew cleanup; brew doctor'
+# Network
+alias localip="ipconfig getifaddr en0"             # Get internal IP address
+alias openports='sudo lsof -i -P -n | grep LISTEN' # List all open ports
+alias ipinfo='curl ipinfo.io'                      # Get IP address information
