beacbef18a0a565ee153b41c1c805775fd8659e6
Carlos Orue <carlos@orue.io>
2024-12-17 16:23:35 -0600

    add to nvm the optioin for .node-version files

--- nvm.zsh
+++ nvm.zsh
@@ -1,19 +1,26 @@
 #!/usr/bin/env zsh
 
 autoload -U add-zsh-hook
-load-nvmrc() {
+
+load-node-version() {
   local nvmrc_path="$(nvm_find_nvmrc)"
-  if [ -n "$nvmrc_path" ]; then
+  local node_version_path="$(pwd)/.node-version"
+
+  if [ -f "$node_version_path" ]; then
+    local node_version=$(cat "$node_version_path")
+    nvm use "$node_version"
+  elif [ -n "$nvmrc_path" ]; then
     local nvmrc_node_version=$(nvm version "$(cat "${nvmrc_path}")")
     if [ "$nvmrc_node_version" = "N/A" ]; then
       nvm install
     elif [ "$nvmrc_node_version" != "$(nvm version)" ]; then
       nvm use
     fi
-  elif [ -n "$(PWD=$OLDPWD nvm_find_nvmrc)" ] && [ "$(nvm version)" != "$(nvm version default)" ]; then
+  elif [ "$(nvm version)" != "$(nvm version default)" ]; then
     echo "Reverting to nvm default version"
     nvm use default
   fi
 }
-add-zsh-hook chpwd load-nvmrc
-load-nvmrc
+
+add-zsh-hook chpwd load-node-version
+load-node-version
