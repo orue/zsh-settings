22c46ca2c37e3cccf6fea643ba32e8d9b905a7ba
Carlos Orue <carlos@orue.io>
2025-10-14 08:32:21 -0500

    Fix Variables file

--- variables.zsh
+++ variables.zsh
@@ -1,20 +1,26 @@
 #!/usr/bin/env zsh
 
+# AWS Configuration
 export AWS_DEFAULT_REGION="us-east-2"
 
-export CO_DEV_PROFILE="default"
-export MJP_DEV_PROFILE="mjp_dev"
+# AWS Profiles
+export CO_DEV_PROFILE="default"    # Default AWS profile
+
+# Load zsh colors
+autoload -U colors && colors
 
 # Regular Colors
-RESET='\033[0m'               # Text Reset
-Black="\[\033[0;30m\]"        # Black
-Red="\[\033[0;31m\]"          # Red
-Green="\[\033[0;32m\]"        # Green
-Yellow="\033[0;33m"           # Yellow
-Blue="\[\033[0;34m\]"         # Blue
-Purple="\[\033[0;35m\]"       # Purple
-Cyan="\[\033[0;36m\]"         # Cyan
-White="\[\033[0;37m\]"        # White
-node_color="\033[0;32m"       # Green
-aws_color='\033[38;5;216m'    # aws
-python_color='\033[38;5;223m' # python
+RESET='\033[0m'
+Black='\033[0;30m'
+Red='\033[0;31m'
+Green='\033[0;32m'
+Yellow='\033[0;33m'
+Blue='\033[0;34m'
+Purple='\033[0;35m'
+Cyan='\033[0;36m'
+White='\033[0;37m'
+
+# Tool-specific colors
+node_color='\033[0;32m'       # Green
+aws_color='\033[38;5;216m'    # Orange
+python_color='\033[38;5;223m' # Light yellow
